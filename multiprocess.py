b0efb576559a678cc0d95f1fb370c1003192f37f
hehonglu123 <hehonglu123@126.com>
2021-03-30 21:14:47 -0400

    double gripper

---
+++ multiprocess.py
@@ -0,0 +1,19 @@
+from multiprocessing import Process
+
+def func1():
+  print ('func1: starting')
+  for i in range(10000000): print('process1:'+str(i))
+  print ('func1: finishing')
+
+def func2():
+  print ('func2: starting')
+  for i in range(10000000): print('process2:'+str(i))
+  print ('func2: finishing')
+
+if __name__ == '__main__':
+  p1 = Process(target=func1)
+  p1.start()
+  p2 = Process(target=func2)
+  p2.start()
+  p1.join()
+  p2.join()
