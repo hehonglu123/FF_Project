4e921909c233784e48cfdc358b20dc73810b2ab7
hehonglu123 <hehonglu123@126.com>
2021-05-01 21:55:57 -0400

    diff robot pose

---
+++ individual_client/jog_debug.py
@@ -0,0 +1,44 @@
+from RobotRaconteur.Client import *
+import sys, time, yaml, traceback
+import numpy as np
+from importlib import import_module
+
+
+
+
+robot_name='abb'
+
+
+sys.path.append('../toolbox')
+inv = import_module(robot_name+'_ik')
+R_ee = import_module('R_'+robot_name)
+
+
+robot_sub=RRN.SubscribeService(url)
+robot=robot_sub.GetDefaultClientWait(1)
+
+
+robot_const = RRN.GetConstants("com.robotraconteur.robotics.robot", robot)
+halt_mode = robot_const["RobotCommandMode"]["halt"]
+jog_mode = robot_const["RobotCommandMode"]["jog"]
+robot.command_mode = halt_mode
+time.sleep(0.1)
+robot.command_mode = jog_mode
+
+num_joints=len(robot.robot_info.joint_info)
+
+
+state_w = robot_sub.SubscribeWire("robot_state")
+time.sleep(0.5)
+robot_state_wire=state_w.TryGetInValue()
+
+robot_state = robot_state_wire[1]
+p=robot_state.kin_chain_tcp[0]['position'] 
+
+
+R=R_ee.R_ee_up(0)
+q=inv.inv(np.array([0.5,0.,0.5]),R)
+robot.jog_freespace(q, np.ones(num_joints), True)
+
+
+
--- toolbox/R_abb.py
+++ toolbox/R_abb.py
@@ -1,6 +1,16 @@
 import numpy as np
-def R_ee(angle):			#need modify
+def R_ee_down(angle):		
 	R=np.array([[0, -np.sin(angle),np.cos(angle)],
 			[0, np.cos(angle), np.sin(angle)],
 			[-1,0,0]])
+	return R
+def R_ee_up(angle):			#need modify
+	R=np.array([[0, np.sin(angle),np.cos(angle)],
+			[0, -np.cos(angle), np.sin(angle)],
+			[1,0,0]])
+	return R
+def R_ee_tilt(angle):			#need modify
+	R=np.array([[0, 0, 1],
+			[np.sin(angle), np.cos(angle), 0],
+			[-np.cos(angle),np.sin(angle),0]])
 	return R
