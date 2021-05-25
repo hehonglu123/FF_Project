452cc02d2216bfa376c85ee01d446fa58ff50cb0
hehonglu123 <hehonglu123@126.com>
2021-05-24 22:14:01 -0400

    template

---
+++ individual_client/match_test.py
@@ -0,0 +1,43 @@
+import cv2, sys
+from matplotlib import pyplot as plt
+import numpy as np
+sys.path.append('../toolbox/')
+from temp_match import match
+
+# im_pth = "image_data/white0.jpg"
+im_pth = "image_data/white1.jpg"
+
+image = cv2.imread(im_pth,cv2.IMREAD_GRAYSCALE)
+template=cv2.imread('../client_yaml/temp1.jpg',cv2.IMREAD_GRAYSCALE)
+w, h = template.shape[::-1]
+
+
+
+# test_region=image[350:652,250:500]
+test_region=image[368:591,800:1050]
+
+
+
+
+# cv2.imshow("image", test_region)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+print(match(test_region,template))
+
+
+
+
+
+# res = cv2.matchTemplate(test_region,template,eval('cv2.TM_SQDIFF'))
+# min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+# top_left=min_loc
+# bottom_right = (top_left[0] + w, top_left[1] + h)
+# print(top_left,bottom_right,min_val)
+# cv2.rectangle(test_region,top_left, bottom_right, 255, 2)
+# plt.subplot(121),plt.imshow(res,cmap = 'gray')
+# plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(test_region,cmap = 'gray')
+# plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+# plt.show()
---
+++ toolbox/temp_match.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2
+# BACKGROUND=[50,98,155]
+BACKGROUND=110
+def rotate_image(mat, angle):
+	"""
+	Rotates an image (angle in degrees) and expands image to avoid cropping
+	"""
+
+	height, width = mat.shape[:2] # image shape has 3 dimensions
+
+	image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+	rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+	# rotation calculates the cos and sin, taking absolutes of those.
+	abs_cos = abs(rotation_mat[0,0]) 
+	abs_sin = abs(rotation_mat[0,1])
+
+	# find the new width and height bounds
+	bound_w = int(height * abs_sin + width * abs_cos)
+	bound_h = int(height * abs_cos + width * abs_sin)
+
+	# subtract old image center (bringing image back to origo) and adding the new image center coordinates
+	rotation_mat[0, 2] += bound_w/2 - image_center[0]
+	rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+	# rotate image with the new bounds and translated rotation matrix
+	rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h),cv2.BORDER_CONSTANT,borderValue=BACKGROUND)
+	return rotated_mat
+def square(im,desired_size):
+	old_size = im.shape[:2] # old_size is in (height, width) format
+
+	ratio = float(desired_size)/max(old_size)
+	new_size = tuple([int(x*ratio) for x in old_size])
+
+	# new_size should be in (width, height) format
+
+	im = cv2.resize(im, (new_size[1], new_size[0]))
+
+	delta_w = desired_size - new_size[1]
+	delta_h = desired_size - new_size[0]
+	top, bottom = delta_h//2, delta_h-(delta_h//2)
+	left, right = delta_w//2, delta_w-(delta_w//2)
+
+	new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,value=BACKGROUND)
+	return new_im
+
+def match(image,template):
+	min_error=9999999999
+	act_angle=0
+	w, h = template.shape[::-1]
+
+	for angle in range(0,360,1):
+		
+		template_rt=rotate_image(template,angle)
+		template_rt=square(template_rt,np.max(np.shape(template_rt)))
+		#matching
+		res = cv2.matchTemplate(image,template_rt,eval('cv2.TM_SQDIFF'))
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+		if min_val<min_error:
+			min_error=min_val
+			act_angle=angle
+			loc=min_loc
+
+	return act_angle,(min_loc[0]+w/2,min_loc[1]+h/2)
+
+
