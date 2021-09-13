7cedb0341fcf0636ac7d9313ea5974c32a58bd8b
hehonglu123 <hehonglu123@126.com>
2021-09-13 15:49:05 -0400

    humidity

--- EA_Gripper/distance_sensor.py
+++ EA_Gripper/distance_sensor.py
@@ -7,7 +7,7 @@ import adafruit_vl53l0x
 
 # Initialize I2C bus and sensor.
 i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l0x.VL53L0X(i2c,address=0x28)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c,address=0x29)
 
 
 while True:
